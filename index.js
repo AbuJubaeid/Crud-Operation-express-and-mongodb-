d95edc11536215054e18ad6bbde9500bffa4b938
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-11-06 04:31:08 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -147,6 +147,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete a bid from mybids
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
     console.log("✅ Routes are ready");
